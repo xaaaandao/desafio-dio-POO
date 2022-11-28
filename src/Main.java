ef01213831880cde6b4b4603c45538535256b241
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-11-28 10:23:14 -0300

    Add classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso java");
+        curso2.setDescricao("descrição curso java");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descrição de mentoria java");
+        mentoria.setData(LocalDate.now());
     }
 }
